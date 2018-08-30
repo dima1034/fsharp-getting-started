2b07309db127ca474d3d708d08ea88634e2259b2
Dima <dima1034@users.noreply.github.com>
2018-08-30 18:59:17 +0300

    choise.fs

--- Choise.fs
+++ Choise.fs
@@ -6,6 +6,9 @@ let div num den =
 let safeDiv num den = 
     try Choice2Of2 (num / den) 
     with 
+        | :? System.DivideByZeroException as dbz -> Choise
         | :? System.Exception as ex -> Choice1Of2 ex
+        
+        // :? Тип исключения	Соответствует указанному типу исключения .NET.
         
 let evaluateMyChoice a b = safeDiv a b 
